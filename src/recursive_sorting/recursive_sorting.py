27cc2b2276b7d477a15dfdd495489bda24ad4dde
Justin Gent <justinbgent@gmail.com>
2020-02-18 15:50:14 -0700

    Re-work and fix solution for helper merge function. Add correct code for merge_sort function

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -3,7 +3,29 @@ def merge( arrA, arrB ):
     elements = len( arrA ) + len( arrB )
     merged_arr = [0] * elements
     # TO-DO
-
+    aIndex = 0
+    bIndex = 0
+    for i in range(0, elements):
+        if (arrA[aIndex] < arrB[bIndex]):
+            merged_arr[i] = arrA[aIndex]
+            if (aIndex + 1 != len(arrA)):
+                aIndex += 1
+            else:
+                for j in range((i + 1), elements):
+                    merged_arr[j] = arrB[bIndex]
+                    bIndex += 1
+                    #if (bIndex == len(arrB)):
+                        #break   don't need this because arrB can't run out of index's before loop finishes
+                break
+        else:
+            merged_arr[i] = arrB[bIndex]
+            if (bIndex + 1 != len(arrB)):
+                bIndex += 1
+            else:
+                for j in range((i + 1), elements):
+                    merged_arr[j] = arrA[aIndex]
+                    aIndex += 1
+                break
     
     return merged_arr
 
@@ -11,7 +33,14 @@ def merge( arrA, arrB ):
 # TO-DO: implement the Merge Sort function below USING RECURSION
 def merge_sort( arr ):
     # TO-DO
+    if len(arr) <= 1:
+        return arr
+    halfSize = int(len(arr) / 2)
+    arr1 = merge_sort(arr[0:halfSize])
+    arr2 = merge_sort(arr[halfSize:])
 
+    arr = merge(arr1, arr2)
+    
     return arr
 
 
@@ -34,6 +63,10 @@ def timsort( arr ):
     return arr
 
 
+# Not Part of Assignment just Fibonacci playing around
+# Breaks when I put anything lower than 2 inside, if
+# statements could solve that. 'n' is for how many
+# numbers in the sequence you want
 def fib_seq(n):
     arr = [0] * n
     if(n == 2):
